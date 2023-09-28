5ac306618e459862c2f4861ff6f25a9d45bcd5b8
Mohammed <faisalshahna38@gmail.com>
2023-09-28 23:15:48 +0530

    Updated real content

--- function.js
+++ function.js
@@ -29,10 +29,6 @@ const typeWriter = () => {
     }
     else if (index === 0 && post==="Video Editor") {
         flag = 0;
-        post = "Photographer";
-    }
-    else if (index === 0 && post==="Photographer") {
-        flag = 0;
         post = "Web Designer";
     }
 
@@ -270,14 +266,14 @@ tl5.from('#counter-4', {
 })
 tl5.add(gsap.delayedCall(-0.1,counter4));
 
-function counter3(){
-    let counts3 = setInterval(updated3);
-    let upto3 = 0;
-    function updated3() {
-        let count3 = document.getElementById("counter-3");
-        count3.innerHTML = ++upto3;
-        if (upto3 === 250) {
-            clearInterval(counts3);
+function counter1(){
+    let counts1 = setInterval(updated1);
+    let upto1 = 0;
+    function updated1() {
+        let count1 = document.getElementById("counter-1");
+        count1.innerHTML = ++upto1;
+        if (upto1 === 1) {
+            clearInterval(counts1);
         }
     }
 }
@@ -288,24 +284,23 @@ function counter2(){
     function updated2() {
         let count2 = document.getElementById("counter-2");
         count2.innerHTML = ++upto2;
-        if (upto2 === 300) {
+        if (upto2 === 1) {
             clearInterval(counts2);
         }
     }
 }
 
-function counter1(){
-    let counts1 = setInterval(updated1);
-    let upto1 = 0;
-    function updated1() {
-        let count1 = document.getElementById("counter-1");
-        count1.innerHTML = ++upto1;
-        if (upto1 === 300) {
-            clearInterval(counts1);
+function counter3(){
+    let counts3 = setInterval(updated3);
+    let upto3 = 0;
+    function updated3() {
+        let count3 = document.getElementById("counter-3");
+        count3.innerHTML = ++upto3;
+        if (upto3 === 20) {
+            clearInterval(counts3);
         }
     }
 }
-
 
 function counter4(){
     let counts4 = setInterval(updated4);
@@ -313,7 +308,7 @@ function counter4(){
     function updated4() {
         let count4 = document.getElementById("counter-4");
         count4.innerHTML = ++upto4;
-        if (upto4 === 450) {
+        if (upto4 === 150) {
             clearInterval(counts4);
         }
     }
